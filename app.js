4a966566167ac64ac64918bdcc357679b73d9090
aniarya82 <hiten.iitkgp@gmail.com>
2018-05-07 15:06:52 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const moment = require('moment');
+const RequestLog = require('./models/request_log');
+
+const app = express();
+require('mongoose').connect('mongodb://localhost/poster');
+
+require('dotenv').config();
+const Pusher = require('pusher');
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_APP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+    cluster: process.env.PUSHER_APP_CLUSTER
+});
+
+app.use((req, res, next) => {
+    let requestTime = Date.now();
+    res.on('finish', () => {
+        if (req.path === '/analytics') {
+            return;
+        }
+
+        RequestLog.create({
+            url: req.path,
+            method: req.method,
+            responseTime: (Date.now() - requestTime) / 1000, // convert to seconds
+            day: moment(requestTime).format("dddd"),
+            hour: moment(requestTime).hour()
+        });
+
+        // trigger a message with the updated analytics
+        require('./analytics_service').getAnalytics()
+            .then(analytics => pusher.trigger('analytics', 'updated', {analytics}));
+    });
+    next();
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+require('hbs').registerHelper('toJson', data => JSON.stringify(data));
+app.set('view engine', 'hbs');
+
+app.get('/analytics', (req, res, next) => {
+    require('./analytics_service').getAnalytics()
+        .then(analytics => res.render('analytics', { analytics }));
+});
+
+app.get('/wait/:seconds', async (req, res, next) => {
+    await ((seconds) => {
+        return new Promise(resolve => {
+            setTimeout(
+                () => resolve(res.send(`Waited for ${seconds} seconds`)),
+                seconds * 1000
+            )
+        });
+    })(req.params.seconds);
+});
+
+module.exports = app;
